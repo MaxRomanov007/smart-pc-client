9337747d6242ec9f4692269ed27742e892285b2d
Max Romanov <maksipapa999@gmail.com>
2025-12-30 16:45:02 +0300

    added smart pc logo

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,9 @@
 import type {Metadata} from 'next';
 import {getExtracted} from "next-intl/server";
-import {Button, HStack, Text} from "@chakra-ui/react";
+import {AbsoluteCenter, HStack, Icon, VStack} from "@chakra-ui/react";
+import RomanovDigitalMiniLogo from "@/components/icons/logo/romanov-digital/MiniLogo";
+import RomanovDigitalFullLogo from "@/components/icons/logo/romanov-digital/FullLogo";
+import SmartPCLogo from "@/components/icons/logo/smart-pc/FullLogo";
 
 export async function generateMetadata(): Promise<Metadata> {
   const t = await getExtracted()
@@ -17,15 +20,32 @@ export default async function LocalePage() {
   const t = await getExtracted()
 
   return (
-    <HStack>
-      <Text>
-        {t({
-          message: "Hello, {name}",
-          values: {name: "John Doe"},
-          description: "index page greeting",
-        })}
-      </Text>
-      <Button>Hello</Button>
-    </HStack>
+    <AbsoluteCenter>
+      <VStack gap={8}>
+        <Icon
+          color={{ _light: "colorPalette.700", _dark: "colorPalette.300" }}
+          h={20}
+          w="auto">
+          <RomanovDigitalFullLogo/>
+        </Icon>
+
+        <HStack gap={8}>
+          <Icon
+            color={{ _light: "colorPalette.700", _dark: "colorPalette.300" }}
+            h={10}
+            w="auto">
+            <RomanovDigitalMiniLogo/>
+          </Icon>
+
+          <Icon
+            color={{ _light: "colorPalette.700", _dark: "colorPalette.300" }}
+            h={10}
+            w="auto">
+            <SmartPCLogo/>
+          </Icon>
+        </HStack>
+      </VStack>
+
+    </AbsoluteCenter>
   );
 };
