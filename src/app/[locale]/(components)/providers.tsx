cad0255b639f49051e41c9f1baa07988baeda275
Max Romanov <maksipapa999@gmail.com>
2025-12-30 10:19:56 +0300

    chakra ui setup

---
+++ src/app/[locale]/(components)/providers.tsx
@@ -0,0 +1,8 @@
+import type {ReactNode} from "react";
+import {Provider} from "@/components/ui/chakra/provider";
+
+export default function Providers({children}: { children: ReactNode }) {
+  return (
+    <Provider>{children}</Provider>
+  );
+};
