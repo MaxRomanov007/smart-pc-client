218b90aa59a836b02aacba5cd2b015cd93ff679a
user <maxipapa999@gmail.com>
2025-12-30 10:00:58 +0300

    setup next intl

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,25 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
 };
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin({
+  experimental: {
+    srcPath: './src',
+
+    extract: {
+      sourceLocale: 'en'
+    },
+
+    messages: {
+      path: './messages',
+      format: 'po',
+      locales: 'infer'
+    }
+  }
+});
+
+export default withNextIntl(nextConfig);
---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,31 @@
+import {type ReactNode} from "react";
+import {routing} from "@/i18n/routing";
+import {setRequestLocale} from "next-intl/server";
+import {notFound} from "next/navigation";
+import {hasLocale} from "next-intl";
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({locale}));
+}
+
+type Props = {
+  children: ReactNode;
+  params: Promise<{ locale: string }>;
+};
+
+export default async function RootLayout({children, params}: Props) {
+  const {locale} = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  setRequestLocale(locale);
+
+  return (
+    <html lang={locale} suppressHydrationWarning>
+    <body>
+    {children}
+    </body>
+    </html>
+  );
+}
