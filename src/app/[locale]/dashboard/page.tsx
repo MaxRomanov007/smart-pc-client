0583134530fe4b84918d5999ee7b81fd55b5b88b
Max Romanov <maksipapa999@gmail.com>
2026-01-11 00:13:59 +0300

    created dashboard page

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -1,9 +1,34 @@
 import type { Metadata } from "next";
+import { Heading, Stack, Text } from "@chakra-ui/react";
+import Breadcrumbs from "@/components/breadcrumbs/breadcrumbs";
+import { getStandardBreadcrumbs } from "@/utils/ui/breadcrumbs/server";
+import { getExtracted } from "next-intl/server";
 
 export const metadata: Metadata = {
   title: "Dashboard",
 };
 
-export default function DashboardPage() {
-  return <div></div>;
+export default async function DashboardPage() {
+  const breadcrumbs = await getStandardBreadcrumbs(true);
+  const t = await getExtracted("dashboard-page");
+
+  return (
+    <Stack gap={4} as="section">
+      <Breadcrumbs items={[breadcrumbs.dashboard]} />
+      <Stack gap={1}>
+        <Heading size="3xl" as="h1">
+          {t({
+            message: "My PCs",
+            description: "dashboard page title",
+          })}
+        </Heading>
+        <Text color="fg.muted">
+          {t({
+            message: "Manage and control your registered computers",
+            description: "dashboard page description",
+          })}
+        </Text>
+      </Stack>
+    </Stack>
+  );
 }
--- src/utils/ui/breadcrumbs/server.ts
+++ src/utils/ui/breadcrumbs/server.ts
@@ -1,12 +1,12 @@
 import { PAGES } from "@/config/navigation/pages";
 import { getExtracted } from "next-intl/server";
 
-export async function getStandardBreadcrumbs() {
+export async function getStandardBreadcrumbs(current: boolean = false) {
   const t = await getExtracted("breadcrumbs");
 
   return {
     dashboard: {
-      href: PAGES.dashboard,
+      href: current ? undefined : PAGES.dashboard,
       label: t({
         message: "Dashboard",
         description: "dashboard breadcrumb",
