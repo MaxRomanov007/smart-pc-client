7dae9fbe0b1ca3ba40e22ec53b8e2cc3858bedbc
Romanov Max <maksipapa999@gmail.com>
2026-01-21 11:45:32 +0300

    docker update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import {defineConfig, globalIgnores} from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import i18nextPlugin from "eslint-plugin-i18next";
@@ -6,9 +6,9 @@ import i18nextPlugin from "eslint-plugin-i18next";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  // Override default ignores of eslint-configs-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
+    // Default ignores of eslint-configs-next:
     ".next/**",
     "out/**",
     "build/**",
